f2ac6fe478ee5480b8d191ff7f704c23566dfed6
melonegoist <408517@niuitmo.ru>
2025-09-10 14:04:38 +0300

    project init

---
+++ is-ticket-service/src/main/java/edu/itmo/isticketservice/IsTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.itmo.isticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsTicketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsTicketServiceApplication.class, args);
+    }
+
+}
---
+++ is-ticket-service/src/test/java/edu/itmo/isticketservice/IsTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itmo.isticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTicketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
