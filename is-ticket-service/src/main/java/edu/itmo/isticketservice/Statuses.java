fe84b13c33ef9330ba85894d730383b4e9b276c5
melonegoist <408517@niuitmo.ru>
2025-09-13 12:00:48 +0300

    persistence-layer: create repositories

---
+++ is-ticket-service/src/main/java/edu/itmo/isticketservice/Statuses.java
@@ -0,0 +1,5 @@
+package edu.itmo.isticketservice;
+
+public enum Statuses {
+
+}
---
+++ is-ticket-service/src/main/java/edu/itmo/isticketservice/model/User.java
@@ -0,0 +1,52 @@
+package edu.itmo.isticketservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+}
